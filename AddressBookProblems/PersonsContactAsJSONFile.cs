d386df64caa2d082f58ccea20b7b2b7c8fa12342
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-08 23:22:44 +0530

    [Jyoti Rao]JSONFile: Read or Write Persons Contact

---
+++ AddressBookProblems/PersonsContactAsJSONFile.cs
@@ -0,0 +1,45 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AddressBookProblems
+{
+    public class PersonsContactAsJSONFile
+    {
+        public static string importfilePath = @"E:\AddressBookProblemsUpdate\Address-Book-Problems\AddressBookProblems\Import.csv";
+        public static string exportfilePath = @"E:\AddressBookProblemsUpdate\Address-Book-Problems\AddressBookProblems\Export.json";
+        public static void ImplementCSVtoJSON()
+        {
+            using (var reader = new StreamReader(importfilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<PersonalDetails>().ToList();
+                Console.WriteLine("Read Data successfully from import.csv file and here data are");
+                foreach (PersonalDetails contact in records)
+                {
+                    Console.Write("\n" + contact.firstName);
+                    Console.Write("\n" + contact.lastName);
+                    Console.Write("\n" + contact.address);
+                    Console.Write("\n" + contact.city);
+                    Console.Write("\n" + contact.state);
+                    Console.Write("\n" + contact.zipcode);
+                    Console.Write("\n" + contact.phonenumber);
+                    Console.Write("\n" + contact.email);
+                    Console.Write("\n");
+                }
+                Console.WriteLine("Read data from csv file Write data to Json File");
+                JsonSerializer serializer=new JsonSerializer();
+                using ( StreamWriter sw = new StreamWriter(exportfilePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
--- AddressBookProblems/Program.cs
+++ AddressBookProblems/Program.cs
@@ -14,7 +14,7 @@ namespace AddressBookProblems
             while (command != "exit")
             {
                 Console.Clear();
-                Console.WriteLine("The commands are:\n 1.create\n 2.list\n 3.edit\n 4.delete\n 5.addMultiContacts\n 6.newUser\n 7.checkDuplicateName\n 8.searchPersonByCityOrState\n 9.ViewPersonByCityOrState\n 10.CountCityOrState\n 11.SortPersonsName\n 12.SortPersonByCityStateOrZip\n 13.checkPersonsContactsFileExists\n 14.readerPersonsContactsFile\n 15.writerPersonsContactsFile\n 16.implementJSONToCSV\n 17.exit\n ");
+                Console.WriteLine("The commands are:\n 1.create\n 2.list\n 3.edit\n 4.delete\n 5.addMultiContacts\n 6.newUser\n 7.checkDuplicateName\n 8.searchPersonByCityOrState\n 9.ViewPersonByCityOrState\n 10.CountCityOrState\n 11.SortPersonsName\n 12.SortPersonByCityStateOrZip\n 13.checkPersonsContactsFileExists\n 14.readerPersonsContactsFile\n 15.writerPersonsContactsFile\n 16.implementJSONToCSV\n 17.personsContactAsJSONFile.ImplementCSVtoJSON\n 18.exit\n ");
                 Console.WriteLine("Please enter a command: ");
                 command = Console.ReadLine().ToLower();
 
@@ -68,6 +68,9 @@ namespace AddressBookProblems
                         break;
                     case "implementJSONToCSV":
                         PersonsContactsAsCSVFile.ImplementJSONToCSV();
+                        break;
+                    case "personsContactAsJSONFile.ImplementCSVtoJSON":
+                        PersonsContactAsJSONFile.ImplementCSVtoJSON();
                         break;
                     default:
                         if (command != "exit")
